e47e2716cb8c1608827cf9535a4478348d501350
sangnq <sangnq@vuthao.com>
2023-08-11 17:59:36 +0700

    set up call api done

--- src/actions/authActions.tsx
+++ src/actions/authActions.tsx
@@ -1,11 +1,8 @@
 import ApiUtils from "../utils/ApiUtils";
 import API_CONFIG from "../config/ApiConfig";
-import { encrypt } from "../utils/CryptUtils";
 
 export const login = async (user: { username: string; password: string; }) => {
-  let data = await encrypt(user);
-  console.log(data);
-  ApiUtils.postPostData(API_CONFIG.getSubSite() + '/api/ApiMobile.ashx?func=AdfsLogin', data);
+  ApiUtils.postPostData(API_CONFIG.getSubSite() + '/api/ApiMobile.ashx?func=AdfsLogin', user);
   return {
     type: user.password == '' || user.username == '' ? 'LOGOUT' : 'LOGIN_SUCCESS',
     payload: user.password == '' || user.username == '' ? null : user,
